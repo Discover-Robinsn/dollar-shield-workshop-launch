d93c3fcbe7a86a35d9603ea3e49c57c5902a8995
Discover-Robinsn <discover.robinsn@gmail.com>
2025-05-05 22:39:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/dollar-shield-workshop-launch/',
   plugins: [
     react(),
     mode === 'development' &&
